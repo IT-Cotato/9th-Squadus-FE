e2e5bfd5a827604d1244a9ca05cd2308afd3bf37
leve68 <hanstom000608@gmail.com>
2024-05-05 19:03:23 +0900

    project init

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:squadus/screen/home_screen.dart';
+import 'package:squadus/screen/sign_in_screen.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: HomeScreen(),
-  ));
+  runApp(
+    MaterialApp(
+      home: SignIn(),
+    ),
+  );
 }
