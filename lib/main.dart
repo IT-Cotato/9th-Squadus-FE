d6c78acfe3b8eeef875e54972f74226c73b29eb9
eVe68 <128668023+leve68@users.noreply.github.com>
2024-05-07 16:50:27 +0900

    Revert "FEAT: kakao 에서 토큰 받아오기"

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:squadus/screen/home_screen.dart';
 import 'package:squadus/screen/sign_in_screen.dart';
-import 'package:kakao_flutter_sdk_common/kakao_flutter_sdk_common.dart';
 
 void main() {
-  // runApp() 호출 전 Flutter SDK 초기화
-  KakaoSdk.init(
-    nativeAppKey: "9a80e6ad2553c99dd8d0dbe467a9bf0c",
-  );
   runApp(
     MaterialApp(
       home: SignIn(),
