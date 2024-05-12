da779c842e0b0c57a1ea640014e7f890720ddb5b
KimDoHyun <kdhqwe1030@naver.com>
2024-05-13 00:58:41 +0900

    [#1] Google OAuth

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:squadus/screen/home_screen.dart';
 import 'package:squadus/screen/sign_in_screen.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
 
 void main() {
+  const List<String> scopes = <String>[
+    'email',
+    'https://www.googleapis.com/auth/contacts.readonly',
+  ];
+
+  GoogleSignIn _googleSignIn = GoogleSignIn(
+    scopes: scopes,
+  );
+
   runApp(
     MaterialApp(
-      home: SignIn(),
+      debugShowCheckedModeBanner: false,
+      home: SignIn(googleSignIn: _googleSignIn),
     ),
   );
 }
--- lib/screen/sign_in_screen.dart
+++ lib/screen/sign_in_screen.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
 
 class SignIn extends StatelessWidget {
-  const SignIn({super.key});
+  final GoogleSignIn googleSignIn;
+  const SignIn({required this.googleSignIn, Key? key}) : super(key: key);
+
+  Future<void> _handleSignIn() async {
+    try {
+      await googleSignIn.signIn();
+    } catch (error) {
+      print(error);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Text("Sign In"),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: _handleSignIn,
+          child: Text('구글 로그인'),
+        ),
       ),
     );
   }
