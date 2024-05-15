7cf6d77f29a9ea89355fb32d27cc38bd887a709c
KimDoHyun <kdhqwe1030@naver.com>
2024-05-15 15:31:09 +0900

    FEAT: google 에서 토큰 받아오기

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:squadus/screen/home_screen.dart';
 import 'package:squadus/screen/sign_in_screen.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
-
 void main() {
   const List<String> scopes = <String>[
     'email',
--- lib/screen/sign_in_screen.dart
+++ lib/screen/sign_in_screen.dart
@@ -1,14 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
-
 class SignIn extends StatelessWidget {
   final GoogleSignIn googleSignIn;
   const SignIn({required this.googleSignIn, Key? key}) : super(key: key);
 
   Future<void> _handleSignIn() async {
     try {
-      await googleSignIn.signIn();
+      // Google 로그인
+      GoogleSignInAccount? googleSignInAccount = await googleSignIn.signIn();
+      if (googleSignInAccount != null) {
+        GoogleSignInAuthentication googleSignInAuthentication =
+            await googleSignInAccount.authentication;
+        print('\n');
+        print("------");
+        print('name = ${googleSignInAccount.displayName}');
+        print('email = ${googleSignInAccount.email}');
+        print('id = ${googleSignInAccount.id}');
+        print("Access Token: ${googleSignInAuthentication.accessToken}");
+        print("ID Token: ${googleSignInAuthentication.idToken}");
+        print("------");
+        print('\n');
+
+        // // 받은 토큰을 사용하여 Firebase에 로그인
+        // OAuthCredential credential = GoogleAuthProvider.credential(
+        //   accessToken: googleSignInAuthentication.accessToken,
+        //   idToken: googleSignInAuthentication.idToken,
+        // );
+        // UserCredential userCredential =
+        //     await FirebaseAuth.instance.signInWithCredential(credential);
+        // User user = userCredential.user!;
+      }
     } catch (error) {
       print(error);
     }
