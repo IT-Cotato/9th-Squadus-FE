9b7aa908022824acdc40793e1a5f5001d903c736
crolvlee <leecrolv@gmail.com>
2024-05-26 02:02:56 +0900

    feat: 주요공지 컴포넌트 구현

---
+++ lib/component/notice_card_component.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class MainNoticeCard extends StatelessWidget {
+  final String noticeTitle;
+  final String noticeContent;
+  final bool isNew;
+
+  const MainNoticeCard({
+    required this.noticeTitle,
+    required this.noticeContent,
+    required this.isNew,
+    super.key
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 80.0,
+      padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 20.0),
+      margin: EdgeInsets.symmetric(vertical: 4.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12.0),
+        boxShadow: [
+          BoxShadow(
+            color: Color(0xFF555BA0).withOpacity(0.23),
+            spreadRadius: 0,
+            blurRadius: 10,
+            offset: Offset(0, 2), // changes position of shadow
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Text(
+                noticeTitle,
+                style: TextStyle(
+                  color: Color(0xFF010E57),
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Spacer(),
+              if (isNew)
+                Container(
+                  width: 12.0,
+                  height: 12.0,
+                  decoration: BoxDecoration(
+                    color: Color(0xFF04CD6D),
+                    shape: BoxShape.circle,
+                  ),
+                )
+            ],
+          ),
+          SizedBox(height: 4.0),
+          Text(
+            noticeContent,
+            style: TextStyle(
+              color: Color(0xFF010E57),
+              fontSize: 12,
+              fontWeight: FontWeight.normal,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
