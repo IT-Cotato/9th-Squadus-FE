a715f575683194d3885c7475737dadd0f0369595
crolvlee <leecrolv@gmail.com>
2024-07-10 04:22:20 +0900

    refactor: bottom navigation bar 컴포넌트 구조 개선 및 CSS 수정

--- src/components/BottomNavigationBar.jsx
+++ src/components/BottomNavigationBar.jsx
@@ -1,29 +1,71 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import { ReactComponent as HomeIcon } from '../assets/icons/home.svg';
+import { ReactComponent as ReportIcon } from '../assets/icons/report.svg';
+import { ReactComponent as GroupIcon } from '../assets/icons/group.svg';
+import { ReactComponent as MatchIcon } from '../assets/icons/match.svg';
+import { ReactComponent as MyPageIcon } from '../assets/icons/mypage.svg';
 
 const NavBar = styled.nav`
-  //position: sticky;
   bottom: 0;
   left: 0;
   width: 100%;
-  height: 30px;
+  height: 64px;
   background-color: white;
-  color: ${({ theme }) => theme.colors.neutral[400]};
   display: flex;
   justify-content: space-around;
-  padding: 0.5rem 0;
-
+  padding: 10px 0;
+  border-top: 1px solid #dcdcdc;  // 임시
+  box-sizing: border-box;
+`;
+const NavItem = styled(Link)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex: 1;
+  text-decoration: none;
+  font-size: 10px;
+  color: ${({ theme, isActive }) => (isActive ? theme.colors.secondary[600] : theme.colors.neutral[400])};
+  
+  svg {
+    margin-bottom: 8px;
+    width: 24px;
+    height: 24px;
+    fill: ${({ theme, isActive }) => (isActive ? theme.colors.secondary[600] : theme.colors.neutral[400])};
+  }
 `;
 
 function BottomNavigationBar() {
+  const location = useLocation();
+  const [activeTab, setActiveTab] = useState(location.pathname);
+
+  useEffect(() => {
+    setActiveTab(location.pathname);
+  }, [location]);
+
   return (
     <NavBar>
-      <Link to="/">홈</Link>
-      <Link to="/report">분석</Link>
-      <Link to="/group">그룹</Link>
-      <Link to="/match">매치</Link>
-      <Link to="/mypage">마이페이지</Link>
+      <NavItem to="/" isActive={activeTab === '/'}>
+        <HomeIcon />
+        홈
+      </NavItem>
+      <NavItem to="/report" isActive={activeTab === '/report'}>
+        <ReportIcon />
+        분석
+      </NavItem>
+      <NavItem to="/group" isActive={activeTab === '/group'}>
+        <GroupIcon />
+        그룹
+      </NavItem>
+      <NavItem to="/match" isActive={activeTab === '/match'}>
+        <MatchIcon />
+        매치
+      </NavItem>
+      <NavItem to="/mypage" isActive={activeTab === '/mypage'}>
+        <MyPageIcon />
+        마이페이지
+      </NavItem>
     </NavBar>
   );
 }
