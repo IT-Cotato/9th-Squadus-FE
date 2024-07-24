b8ce27699a8c8f9ad89edb3fdfa95bfc8685a6f4
KimDoHyun <kdhqwe1030@naver.com>
2024-07-24 11:03:31 +0900

    feat: 그룹 기본정보 페이지 레이아웃 구현

--- src/pages/group/basicinfo/BasicInfo.jsx
+++ src/pages/group/basicinfo/BasicInfo.jsx
@@ -1,9 +1,31 @@
+import styled from 'styled-components';
+import ClubMainInfo from './group_components/ClubMainInfo';
+import ClubSubInfo from './group_components/ClubSubInfo';
+
+const BaseContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  padding: 16px 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  box-sizing: border-box;
+`;
+
 const BasicInfo = () => {
   return (
-    <div>
-      <h1>기본정보</h1>
-    </div>
+    <BaseContainer>
+      <ClubMainInfo
+        region={'서울'}
+        personality={'모두 환영'}
+        name={'중앙 가르드'}
+        memRecent={32}
+        memMax={40}
+        establishDate={'2023.09.01'}
+      />
+      <ClubSubInfo />
+    </BaseContainer>
   );
-}
+};
 
 export default BasicInfo;
---
+++ src/pages/group/group_components/ClubSubInfo.jsx
@@ -0,0 +1,66 @@
+import ClubComment from './ClubComment';
+import styled from 'styled-components';
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  box-sizing: border-box;
+`;
+
+const RankContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 12px;
+  gap: 10px;
+  width: 100%;
+  background: linear-gradient(270deg, #1e58ec 0%, #525e9d 100%);
+  box-sizing: border-box;
+
+  box-shadow: 0px 0px 8px rgba(110, 110, 110, 0.23);
+  border-radius: 12px;
+`;
+
+const RankImg = styled.div`
+  width: 83.19px;
+  height: 83.19px;
+  box-shadow: 0px 0px 11px 0px #00fff033;
+`;
+const RankTextContainer = styled.div`
+  width: 100%;
+  box-sizing: border-box;
+`;
+const TextSmall = styled.div`
+  font-size: 12px;
+  font-weight: 400;
+  line-height: 18px;
+  letter-spacing: -0.011em;
+  color: #f9fafb;
+`;
+
+const TextBIg = styled.div`
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 19.2px;
+  letter-spacing: -0.011em;
+  color: #ffffff;
+`;
+const ClubSubInfo = () => {
+  return (
+    <Container>
+      <RankContainer>
+        <RankImg />
+        <RankTextContainer>
+          <TextSmall>티어 정보</TextSmall>
+          <TextBIg>실버</TextBIg>
+          <TextBIg>138팀 중 7위</TextBIg>
+          <TextSmall>다음 레벨까지 남은 순위 : 3위</TextSmall>
+        </RankTextContainer>
+      </RankContainer>
+      <ClubComment title={'동아리 한마디'} content={'화이팅!!'} />{' '}
+    </Container>
+  );
+};
+
+export default ClubSubInfo;
