ad1fa1ba17fb2de9d4e81041a715ddc1068fb2db
crolvlee <leecrolv@gmail.com>
2024-07-28 18:53:28 +0900

    feat: 로그인 CSS

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,24 +1,39 @@
 import styled from 'styled-components';
 import GoogleLoginButton from './GoogleLoginButton';
+import logo from '../../assets/login_logo.svg'
+
 
 const Container = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
-  background-color: orange;
+  background: linear-gradient(90deg, #FF6330 0%, #FF3F00 100%);
+`;
+
+const Logo = styled.div`
+  width: 200px;
+  height: 260px;
+  background-image: url(${logo});
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+  margin: 260px auto 100px;
 `;
 
 const ButtonContainer = styled.div`
   display: flex;
   flex-direction: column;
-  background-color: yellow;
+  /* background-color: yellow; */
+  margin-top: 100px;
+  margin: 0px auto;
+
 `;
 
 
 const Login = () => {
   return (
     <Container>
-      <div>Squadus 로그인 화면!</div>
+      <Logo />
       <ButtonContainer>
         <GoogleLoginButton />
       </ButtonContainer>
