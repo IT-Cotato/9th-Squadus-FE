1d60e98f0f4f79ece228b038d64b15059b86e298
leve68 <hanstom000608@gmail.com>
2024-08-03 23:22:12 +0900

    feat: article component 수정

--- src/pages/home/ArticleDetailList.jsx
+++ src/pages/home/ArticleDetailList.jsx
@@ -8,28 +8,27 @@ const ArticleDetailList = () => {
       id: "1",
       group: "분류 1",
       articleList: [
-        { id: "1", title: "아티클 제목", subTitle: "아티클 소제목" },
-        { id: "2", title: "아티클 제목", subTitle: "아티클 소제목" },
-        { id: "3", title: "아티클 제목", subTitle: "아티클 소제목" },
-        { id: "4", title: "아티클 제목", subTitle: "아티클 소제목" },
+        { img: "", id: "1", title: "아티클 제목", subTitle: "아티클 소제목" },
+        { img: "", id: "2", title: "아티클 제목", subTitle: "아티클 소제목" },
+        { img: "", id: "3", title: "아티클 제목", subTitle: "아티클 소제목" },
+        { img: "", id: "4", title: "아티클 제목", subTitle: "아티클 소제목" },
       ],
     },
     {
       id: "2",
       group: "분류 2",
       articleList: [
-        { id: "5", title: "아티클 제목", subTitle: "아티클 소제목" },
-        { id: "6", title: "아티클 제목", subTitle: "아티클 소제목" },
-        { id: "7", title: "아티클 제목", subTitle: "아티클 소제목" },
+        { img: "", id: "5", title: "아티클 제목", subTitle: "아티클 소제목" },
+        { img: "", id: "6", title: "아티클 제목", subTitle: "아티클 소제목" },
       ],
     },
     {
       id: "3",
       group: "분류 3",
       articleList: [
-        { id: "8", title: "아티클 제목", subTitle: "아티클 소제목" },
-        { id: "9", title: "아티클 제목", subTitle: "아티클 소제목" },
-        { id: "10", title: "아티클 제목", subTitle: "아티클 소제목" },
+        { img: "", id: "8", title: "아티클 제목", subTitle: "아티클 소제목" },
+        { img: "", id: "9", title: "아티클 제목", subTitle: "아티클 소제목" },
+        { img: "", id: "10", title: "아티클 제목", subTitle: "아티클 소제목" },
       ],
     },
   ];
@@ -39,19 +38,34 @@ const ArticleDetailList = () => {
       <ArticleCategoryTitle>{articleData[0].group}</ArticleCategoryTitle>
       <ArticleContainerRow>
         {articleData[0].articleList.map((article) => (
-          <MainArticleItem key={article.id}></MainArticleItem>
+          <MainArticleItem
+            key={article.id}
+            img={article.img}
+            title={article.title}
+            subtitle={article.subTitle}
+          ></MainArticleItem>
         ))}
       </ArticleContainerRow>
       <ArticleCategoryTitle>{articleData[1].group}</ArticleCategoryTitle>
       <ArticleContainerCol>
         {articleData[1].articleList.map((article) => (
-          <MainArticleItem key={article.id}></MainArticleItem>
+          <MainArticleItem
+            key={article.id}
+            img={article.img}
+            title={article.title}
+            subtitle={article.subTitle}
+          ></MainArticleItem>
         ))}
       </ArticleContainerCol>
       <ArticleCategoryTitle>{articleData[2].group}</ArticleCategoryTitle>
       <ArticleContainerRow>
         {articleData[2].articleList.map((article) => (
-          <MainArticleItem key={article.id}></MainArticleItem>
+          <MainArticleItem
+            key={article.id}
+            img={article.img}
+            title={article.title}
+            subtitle={article.subTitle}
+          ></MainArticleItem>
         ))}
       </ArticleContainerRow>
     </Container>
@@ -65,7 +79,9 @@ const Container = styled.div`
   flex-direction: column;
 `;
 
-const ArticleCategoryTitle = styled.div``;
+const ArticleCategoryTitle = styled.div`
+  padding: 16px 20px;
+`;
 
 const ArticleContainerRow = styled.div`
   padding: 8px;
