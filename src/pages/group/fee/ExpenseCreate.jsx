c41bc8a9458ee9a55b7dc1dc3515e0d078649013
crolvlee <leecrolv@gmail.com>
2024-08-05 19:20:19 +0900

    feat: 회비사용내역등록 보완

--- src/pages/group/fee/ExpenseCreate.jsx
+++ src/pages/group/fee/ExpenseCreate.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import CustomCalendar from './fee_components/CustomCalendar';
 
 const Container = styled.div`
   width: 100%;
@@ -53,45 +54,96 @@ const ContentContainer = styled.div`
   padding: 20px;
 `;
 
-const InputContainer = styled.input`
-  box-sizing: border-box;
-  width: 100%;
-  padding: 8px;
+const FieldContainer = styled.div`
+  display: flex;
+  align-items: center;
   margin-bottom: 10px;
+  width: 100%;
+  position: relative;
+`;
+
+const Label = styled.div`
+  font-size: 18px;
+  color: ${({ theme }) => theme.colors.neutral[400]};
+  width: 40%;
+`;
+
+const Input = styled.input`
+  width: 60%;
+  padding: 10px;
   border: none;
   border-radius: 8px;
   background-color: ${({ theme }) => theme.colors.neutral[100]};
   color: ${({ theme }) => theme.colors.neutral[700]};
-  font-size: 18px;
-
-  &::placeholder {
-    color: ${({ theme }) => theme.colors.neutral[400]};
+  font-size: 16px;
+  /* input 타입이 number일 때 스타일 */
+  &[type='number'] {
+    appearance: none;
+    -moz-appearance: textfield; /* 파이어폭스에서 스피너 제거 */
+    -webkit-appearance: none;
+  }
+  &::-webkit-inner-spin-button,
+  &::-webkit-outer-spin-button {
+    -webkit-appearance: none; /* 크롬, 사파리에서 스피너 제거 */
+    margin: 0;
   }
 `;
 
 
 
 const ExpenseCreate = ({ closeExpenseCreate }) => {
+  const [expenseDate, setExpenseDate] = useState(new Date());
+  const [showCalendar, setShowCalendar] = useState(false);
+
+  const handleContainerClick = () => {
+    if (showCalendar) setShowCalendar(false);
+  };
+
   return (
-    <Container>
+    <Container onClick={handleContainerClick}>
       <HeaderContainer>
         <CloseButton onClick={closeExpenseCreate}/>
         <HeaderTitle>회비 사용 내역 등록</HeaderTitle>
         <SaveButton>등록</SaveButton>
       </HeaderContainer>
       <ContentContainer>
-        <InputContainer
-          type="text"
-          placeholder="사용 내역"
-        />
-        <InputContainer
-          type="date"
-          placeholder="회비 사용 일자"
-        />
-        <InputContainer
-          type="text"
-          placeholder="금액"
-        />
+        <FieldContainer>
+          <Label>사용 내역</Label>
+          <Input type="text" />
+        </FieldContainer>
+        <FieldContainer>
+          <Label>회비 사용 일자</Label>
+          <Input
+            readOnly
+            value={expenseDate ? expenseDate.toLocaleDateString() : "날짜 선택"}
+            onClick={(e) => {
+              e.stopPropagation();
+              setShowCalendar(!showCalendar);
+            }}
+          />
+          {showCalendar && (
+            <CustomCalendar
+              value={expenseDate}
+              onChange={(date) => {
+                setExpenseDate(date);
+                setShowCalendar(false);
+              }}
+            />
+          )}
+        </FieldContainer>
+        <FieldContainer>
+          <Label>금액 (원)</Label>
+          <Input
+            type="number"
+            min="0"  // 최소값을 0으로 설정
+            placeholder="숫자로 입력하세요"
+            onChange={e => {
+              if (!Number(e.target.value) && e.target.value !== '') {
+                e.target.value = e.target.value.slice(0, -1);
+              }
+            }}
+          />
+        </FieldContainer>
       </ContentContainer>
 
     </Container>
