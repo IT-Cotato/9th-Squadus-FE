86a154bac91c24f384639f9cb2798d3fc3775653
crolvlee <leecrolv@gmail.com>
2024-08-05 16:48:46 +0900

    feat: 회비 등록 react-calendar 적용

--- src/pages/group/fee/FeeCreate.jsx
+++ src/pages/group/fee/FeeCreate.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import FeeMemberSelect from './FeeMemberSelect';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 const Container = styled.div`
   width: 100%;
@@ -54,37 +56,136 @@ const ContentContainer = styled.div`
   padding: 20px;
 `;
 
-const InputContainer = styled.input`
-  box-sizing: border-box;
-  width: 100%;
-  padding: 8px;
+const FieldContainer = styled.div`
+  display: flex;
+  align-items: center;
   margin-bottom: 10px;
+  width: 100%;
+  position: relative;
+`;
+
+const Label = styled.div`
+  font-size: 18px;
+  color: ${({ theme }) => theme.colors.neutral[400]};
+  width: 40%;
+`;
+
+const Input = styled.input`
+  width: 60%;
+  padding: 10px;
   border: none;
   border-radius: 8px;
   background-color: ${({ theme }) => theme.colors.neutral[100]};
   color: ${({ theme }) => theme.colors.neutral[700]};
-  font-size: 18px;
-
-  &::placeholder {
-    color: ${({ theme }) => theme.colors.neutral[400]};
+  font-size: 16px;
+  /* input 타입이 number일 때 스타일 */
+  &[type='number'] {
+    appearance: none;
+    -moz-appearance: textfield; /* 파이어폭스에서 스피너 제거 */
+    -webkit-appearance: none;
+  }
+  &::-webkit-inner-spin-button,
+  &::-webkit-outer-spin-button {
+    -webkit-appearance: none; /* 크롬, 사파리에서 스피너 제거 */
+    margin: 0;
   }
 `;
 
-const MemberSelectContainer = styled.div`
-  box-sizing: border-box;
-  width: 100%;
-  padding: 12px;
-  margin-bottom: 10px;
-  border-radius: 8px;
-  background-color: ${({ theme }) => theme.colors.neutral[100]};
-  color: ${({ theme }) => theme.colors.neutral[700]};
-  font-size: 18px;
+const CalendarContainer = styled.div`
+  top: 100%;
+  position: absolute;
+  right: 0;
+  z-index: 100;
 `;
 
-const FeeCreate = ({ closeFeeCreate }) => {
-  const [showSelectFeeMemeber, setShowSelectFeeMemeber] = useState(false);
-  const [selectedMemberIds, setSelectedMemberIds] = useState([]);
+const StyledCalendar = styled(Calendar)`
+  background-color: #fff;
+  border: 1px solid ${({ theme }) => theme.colors.neutral[300]};
+  border-radius: 8px;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
 
+  .react-calendar__navigation {
+    span {
+      font-size: 16px;
+      font-weight: 600;
+      color: ${({ theme }) => theme.colors.main[500]};
+    }
+  }
+
+  .react-calendar__navigation button:enabled:focus{
+    width: 15px;
+    background: ${({ theme }) => theme.colors.neutral[100]};
+  }
+
+  .react-calendar__tile {
+    padding: 8px;
+    abbr {
+      color: ${({ theme }) => theme.colors.neutral[500]};
+      text-decoration: none;
+    }
+  }
+
+  .react-calendar__tile--hasActive {
+    background: ${({ theme }) => theme.colors.main[500]};
+    border-radius: 12px;
+
+    abbr {
+      color: white;
+      text-decoration: none;
+    }
+  }
+
+  .react-calendar__tile--now {
+    background: ${({ theme }) => theme.colors.neutral[100]};
+    border-radius: 12px;
+
+    abbr {
+      color: ${({ theme }) => theme.colors.neutral[500]};
+      text-decoration: none;
+    }
+  }
+
+  .react-calendar__tile--active {
+    background: ${({ theme }) => theme.colors.main[500]};
+    border-radius: 12px;
+
+    abbr {
+      color: white;
+      text-decoration: none;
+    }
+  }
+
+  .react-calendar__tile:enabled:hover,
+  .react-calendar__tile:enabled:focus {
+    background: ${({ theme }) => theme.colors.main[100]};
+    border-radius: 12px;
+
+    abbr {
+      color: ${({ theme }) => theme.colors.neutral[500]};
+      text-decoration: none;
+    }
+  }
+
+  /* 저번 달 & 다음 달 일자 */
+  .react-calendar__month-view__days__day--neighboringMonth{
+    abbr {
+      color: ${({ theme }) => theme.colors.neutral[300]};
+    }
+  }
+
+
+
+`;
+
+
+const FeeCreate = ({ closeFeeCreate }) => {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [showStartCalendar, setShowStartCalendar] = useState(false);
+  const [showEndCalendar, setShowEndCalendar] = useState(false);
+
+  const [showSelectFeeMember, setShowSelectFeeMember] = useState(false);
+  const [selectedMemberIds, setSelectedMemberIds] = useState([]);
   const updateSelection = (selectedIds) => {
     setSelectedMemberIds(selectedIds);
   };
@@ -97,32 +198,81 @@ const FeeCreate = ({ closeFeeCreate }) => {
         <SaveButton>완료</SaveButton>
       </HeaderContainer>
       <ContentContainer>
-        <InputContainer
-          type="text"
-          placeholder="회비명"
-        />
-        <InputContainer
-          type="text"
-          placeholder="금액"
-        />
-        <InputContainer
-          type="text"
-          placeholder="회비 구분"
-        />
-        <InputContainer
-          type="date"
-        />
-        <MemberSelectContainer onClick={() => {setShowSelectFeeMemeber(true);}}>
-          회비 납부 인원 선택 ({selectedMemberIds.length}명)
-        </MemberSelectContainer>
-        <InputContainer
-          type="text"
-          placeholder="메모"
-        />
+        <FieldContainer>
+          <Label>회비명</Label>
+          <Input type="text" />
+        </FieldContainer>
+        <FieldContainer>
+          <Label>금액 (원)</Label>
+          <Input
+            type="number"
+            min="0"  // 최소값을 0으로 설정
+            placeholder="숫자로 입력하세요"
+            onChange={e => {
+              if (!Number(e.target.value) && e.target.value !== '') {
+                e.target.value = e.target.value.slice(0, -1);
+              }
+            }}
+          />
+        </FieldContainer>
+        <FieldContainer>
+          <Label>회비 구분</Label>
+          <Input type="text" />
+        </FieldContainer>
+        <FieldContainer>
+          <Label>회비 납부 시작일</Label>
+          <Input
+            readOnly
+            value={startDate ? startDate.toLocaleDateString() : "날짜 선택"}
+            onClick={() => setShowStartCalendar(!showStartCalendar)}
+          />
+          {showStartCalendar && (
+            <CalendarContainer>
+              <StyledCalendar
+                onChange={(date) => {
+                  setStartDate(date);
+                  setShowStartCalendar(!showStartCalendar)
+                }}
+                value={startDate}
+                formatDay={(locale, date) => date.toLocaleString("en", {day: "numeric"})}
+              />
+            </CalendarContainer>
+          )}
+        </FieldContainer>
+
+        <FieldContainer>
+          <Label>회비 납부 마감일</Label>
+          <Input
+            readOnly
+            value={endDate ? endDate.toLocaleDateString() : "날짜 선택"}
+            onClick={() => setShowEndCalendar(!showEndCalendar)}
+          />
+          {showEndCalendar && (
+            <CalendarContainer>
+              <StyledCalendar
+                onChange={(date) => {
+                  setEndDate(date);
+                  setShowEndCalendar(!showEndCalendar)
+                }}
+                value={endDate}
+                formatDay={(locale, date) => date.toLocaleString("en", {day: "numeric"})}
+              />
+            </CalendarContainer>
+          )}
+        </FieldContainer>
+
+        <FieldContainer onClick={() => setShowSelectFeeMember(true)}>
+          <Label>회비 납부 인원</Label>
+          <Input readOnly value={`${selectedMemberIds.length}명`} />
+        </FieldContainer>
+        <FieldContainer>
+          <Label>메모</Label>
+          <Input type="text" />
+        </FieldContainer>
       </ContentContainer>
 
       {
-        showSelectFeeMemeber &&
+        showSelectFeeMember &&
         <div
           style={{
             position: 'fixed',
@@ -143,7 +293,7 @@ const FeeCreate = ({ closeFeeCreate }) => {
               backgroundColor: 'white',
             }}>
             <FeeMemberSelect 
-              closeFeeMemberSelect={() => setShowSelectFeeMemeber(false)} 
+              closeFeeMemberSelect={() => setShowSelectFeeMember(false)} 
               updateSelection={updateSelection}
               selectedMemberIds={selectedMemberIds}
             />
